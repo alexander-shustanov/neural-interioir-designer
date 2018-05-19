41aa37bdd373a87aef624bd787a5c9572aa64cc7
Alexander Shustanov <alexander.shustanov@gmail.com>
2018-05-19 09:57:21 +0400

    Working style transfer

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import glob
 import tkinter as tk
 import tkinter.filedialog as fdialog
+from os.path import basename
 
 import numpy as np
 from PIL import Image, ImageTk
@@ -8,12 +10,15 @@ from skimage.measure import label
 from skimage.transform import resize
 
 from nn import Model, style_angle
+from style_helper import StyleHelper
 
 BRUSH = "brush"
 MAGIC = "magic"
 STYLE = "style"
 
-IMG_SIZE = (300, 300)
+IMG_SIZE = (250, 250)
+
+THR = 0.2
 
 TOOLS = [BRUSH, MAGIC, STYLE]
 
@@ -23,6 +28,7 @@ class App:
         self.model = Model()
 
         self.root = tk.Tk()
+        self.root.config()
 
         self.current_tool = tk.StringVar()
         self.current_tool.set(BRUSH)
@@ -30,22 +36,32 @@ class App:
         self.menu = self.create_menu()
         self.tool_panel = self.create_tool_panel(self.current_tool)
         self.canvas = self.create_canvas()
-        self.canvas_state = CanvasState(self.canvas, self.model)
+        self.canvas_state = CanvasState(self.canvas, self.model, self.on_mask_changed)
 
         self.styles_box = self.create_styles_box()
+
+        self.root.grid_rowconfigure(0, weight=0)
 
         self.root.config(menu=self.menu)
         self.tool_panel.grid(row=0, column=0)
         self.canvas.grid(row=0, column=1)
         self.styles_box.grid(row=0, column=2)
 
+        self.tool_changed()
+
+        self.keys = KeysHelper(self.root)
+
         self.root.mainloop()
+
+    def on_mask_changed(self):
+        self.style_helper = None
 
     def create_tool_panel(self, tool_var):
         tool_panel = tk.Frame(self.root, height=30)
 
         for i, tool in enumerate(TOOLS):
-            tk.Radiobutton(tool_panel, text=tool, variable=tool_var, value=tool).grid(sticky='W', row=i, column=0)
+            tk.Radiobutton(tool_panel, text=tool, variable=tool_var, value=tool, command=self.tool_changed) \
+                .grid(sticky='W', row=i, column=0)
         return tool_panel
 
     def create_canvas(self):
@@ -75,20 +91,43 @@ class App:
         imsave
 
     def create_styles_box(self):
+        styles = sorted(glob.glob("styles/*.*"))
+
         box = tk.Frame(self.root)
 
-        scrollbar = tk.Scrollbar(box)
-        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        scrollbar = tk.Scrollbar(box, orient=tk.VERTICAL)
 
-        listbox = tk.Listbox(box)
-        listbox.pack()
+        listbox = tk.Listbox(box, selectmode=tk.SINGLE)
+        listbox.pack(side=tk.LEFT)
+        scrollbar.pack(side=tk.LEFT, fill=tk.Y)
 
-        for i in range(100):
-            listbox.insert(tk.END, i)
+        for style in styles:
+            listbox.insert(tk.END, basename(style))
 
         # attach listbox to scrollbar
         listbox.config(yscrollcommand=scrollbar.set)
         scrollbar.config(command=listbox.yview)
+
+        style_image_label = tk.Label(box)
+        style_image_label.pack()
+
+        apply_button = tk.Button(box, text="Apply style", command=self.apply_style)
+        apply_button.pack()
+
+        def on_select(event):
+            self.style_helper = None
+
+            selected = int(listbox.curselection()[0])
+
+            self.style_image_pil = Image.open(styles[selected]).resize(IMG_SIZE)
+            self.style_photo = ImageTk.PhotoImage(image=self.style_image_pil)
+
+            style_image_label.configure(image=self.style_photo)
+            style_image_label.image = self.style_photo
+
+            self.style_image_name = styles[selected]
+
+        listbox.bind('<<ListboxSelect>>', on_select)
 
         return box
 
@@ -100,6 +139,26 @@ class App:
 
         if self.tool == MAGIC:
             self.magic_selection(x, y)
+
+    def tool_changed(self):
+        self.styles_box.grid_remove()
+
+        if self.tool == STYLE:
+            self.styles_box.grid()
+
+    def apply_style(self):
+        if self.style_helper is None:
+            style_image = resize(imread(self.style_image_name), IMG_SIZE)
+            self.style_helper = StyleHelper(self.model, self.canvas_state.img, style_image, np.where(self.canvas_state.mask > 0.0, 1.0, 0.0))
+
+        mixed = self.canvas_state.img.copy().astype(float)
+        for i in range(20):
+            mixed -= self.style_helper.step(mixed)
+            mixed = np.clip(mixed, 0.0, 255.0)
+
+        self.canvas_state.img = mixed.astype(np.uint8)
+
+        self.canvas_state.render_image()
 
     @property
     def tool(self):
@@ -118,7 +177,10 @@ class App:
             for j in range(signature_shape[1] - 1):
                 mask[i, j] = style_angle(signature[i, j], selected_signature)
 
-        threshold = np.where(mask < 0.18)
+        threshold = np.where(mask < THR)
+
+        if len(threshold[0]) == 0:
+            return
 
         selected_signature = signature[threshold[0], threshold[1]].mean(axis=0)
 
@@ -127,19 +189,27 @@ class App:
             for j in range(signature_shape[1] - 1):
                 mask[i, j] = style_angle(signature[i, j], selected_signature)
 
-        mask = np.where(mask > 0.18, 0.0, 1.0)
+        mask = np.where(mask > THR, 0.0, 1.0)
 
         labeled = label(mask)
 
-        mask = np.where(labeled != labeled[s_y, s_x], 0.0, 1.0)
+        mask = np.where(labeled != labeled[min(s_y, mask.shape[0] - 1), min(s_x, mask.shape[1] - 1)], 0.0, 1.0)
 
-        self.canvas_state.update_mask(resize(mask, self.canvas_state.mask.shape))
+        mask = resize(mask, self.canvas_state.mask.shape)
+
+        if self.keys.ctrl:
+            self.canvas_state.add_mask(mask)
+        elif self.keys.alt:
+            self.canvas_state.remove_mask(mask)
+        else:
+            self.canvas_state.update_mask(mask)
 
         # noinspection PyAttributeOutsideInit
 
 
 class CanvasState:
-    def __init__(self, canvas, model):
+    def __init__(self, canvas, model, mask_changed_handler):
+        self.mask_changed_handler = mask_changed_handler
         self.model = model
         self.canvas = canvas
 
@@ -152,6 +222,8 @@ class CanvasState:
                                         self.img.shape[:2])
 
         self.render_image()
+
+        self.mask_changed_handler()
 
     def render_image(self):
         selector = np.where(self.mask > 0.0)
@@ -170,9 +242,56 @@ class CanvasState:
         sy = np.clip(sy, 0, self.signature.shape[0] - 1)
         return int(sx), int(sy)
 
+    def add_mask(self, mask):
+        self.mask[np.where(mask > 0.0)] = 1.0
+        self.render_image()
+
+    def remove_mask(self, mask):
+        self.mask[np.where(mask > 0.0)] = 0.0
+        self.render_image()
+
     def update_mask(self, mask):
         self.mask = mask
         self.render_image()
+
+    @property
+    def is_mask_empty(self):
+        return len(np.where(self.mask > 0.0)[0]) == 0
+
+
+class KeysHelper:
+    def __init__(self, root):
+        self._alt = False
+        self._ctrl = False
+
+        root.bind("<Alt_L>", self.on_alt)
+        root.bind("<Control_L>", self.on_ctrl)
+        root.bind("<KeyRelease-Alt_L>", self.out_alt)
+        root.bind("<KeyRelease-Control_L>", self.out_ctrl)
+
+    def on_alt(self, event):
+        self._alt = True
+        print("Press Alt")
+
+    def on_ctrl(self, event):
+        self._ctrl = True
+        print("Press Ctrl")
+
+    def out_alt(self, event):
+        self._alt = False
+        print("Release Alt")
+
+    def out_ctrl(self, event):
+        self._ctrl = False
+        print("Release Ctrl")
+
+    @property
+    def ctrl(self):
+        return self._ctrl
+
+    @property
+    def alt(self):
+        return self._alt
 
 
 app = App()
---
+++ style_helper.py
@@ -0,0 +1,48 @@
+import numpy as np
+import tensorflow as tf
+
+weight_content = 1.5
+weight_style = 10.0
+weight_denoise = 0.3
+num_iterations = 60
+step_size = 2.0
+
+
+class StyleHelper:
+    def __init__(self, model, content_image, style_image, mask):
+        self.model = model
+
+        loss_content = self.model.create_content_loss(content_image, [2])
+        loss_style = self.model.create_masked_style_loss(style_image, [2, 3, 4], mask)
+        loss_denoise = self.model.create_denoise_loss()
+
+        adj_content = tf.Variable(1e-10, name='adj_content')
+        adj_style = tf.Variable(1e-10, name='adj_style')
+        adj_denoise = tf.Variable(1e-10, name='adj_denoise')
+
+        model.sess.run([adj_content.initializer,
+                        adj_style.initializer,
+                        adj_denoise.initializer])
+
+        update_adj_content = adj_content.assign(1.0 / (loss_content + 1e-10))
+        update_adj_style = adj_style.assign(1.0 / (loss_style + 1e-10))
+        update_adj_denoise = adj_denoise.assign(1.0 / (loss_denoise + 1e-10))
+
+        loss_combined = weight_content * adj_content * loss_content + \
+                        weight_style * adj_style * loss_style + \
+                        weight_denoise * adj_denoise * loss_denoise
+
+        gradient = tf.gradients(loss_combined, model.input_image)
+
+        self.run_list = [gradient, update_adj_content, update_adj_style, update_adj_denoise]
+
+    def step(self, img):
+        feed_dict = self.model.create_feed_dict(img)
+
+        grad, adj_content_val, adj_style_val, adj_denoise_val = self.model.sess.run(self.run_list, feed_dict=feed_dict)
+
+        grad = np.squeeze(grad)
+
+        step_size_scaled = step_size / (np.std(grad) + 1e-8)
+
+        return grad * step_size_scaled
